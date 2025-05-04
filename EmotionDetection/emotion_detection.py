eb2b1288ce3b160a209e22b61504a466d12e6817
HarusHoChiHo <harusho@pm.me>
2025-05-04 19:31:39 -0400

    Test

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -1,6 +1,10 @@
 import requests
 
 def emotion_detector(text_to_analyse):
+
+    if not text_to_analyse:
+        return None
+
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
     headers = {
         "grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock",
@@ -23,7 +27,7 @@ def emotion_detector(text_to_analyse):
             name = i
     
     result["dominant_emotion"] = name
-
+    print(result)
     return result
     
     
--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+"""Flask application for detecting emotions from text using Watson NLP."""
 from flask import Flask
 from EmotionDetection.emotion_detection import emotion_detector
 
@@ -5,7 +6,26 @@ app = Flask(__name__)
 
 @app.route("/emotionDetector")
 def emotion_detect():
-    result = emotion_detector("I think I am having fun")
-    text_result = f"For the given statement, the system response is 'anger': {result['anger']}, 'disgust': {result['disgust']}, 'fear': {result['fear']}, 'joy': {result['joy']} and 'sadness': {result['sadness']}. The dominant emotion is {result['dominant_emotion']}."
-    return text_result
+    """Handles POST requests to detect emotion from input text."""
+    result = emotion_detector("")
+
+    if result is None:
+        result = {
+            'anger': None, 
+        'disgust': None, 
+        'fear': None, 
+        'joy': None, 
+        'sadness': None, 
+        'dominant_emotion': None}
+
+    if result['dominant_emotion'] is None:
+        return "Invalid text! Please try again!", 400
+
+    text_result1 = "For the given statement, "
+    text_result2 = f"the system response is 'anger': {result['anger']}, "
+    text_result3 = f"'disgust': {result['disgust']}, "
+    text_result4 = f"'fear': {result['fear']}, "
+    text_result5 = f"'joy': {result['joy']} and 'sadness': {result['sadness']}. "
+    text_result6 = f"The dominant emotion is {result['dominant_emotion']}."
+    return text_result1+text_result2+text_result3+text_result4+text_result5+text_result6
     
